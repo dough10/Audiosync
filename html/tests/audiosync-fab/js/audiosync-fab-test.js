c8b56c257e3c794499459f40f8d7cca7b25eb3bd
dough10 <doughten.jimi@gmail.com>
2024-05-11 06:08:38 -0500

    test states

--- html/tests/audiosync-fab/js/audiosync-fab-test.js
+++ html/tests/audiosync-fab/js/audiosync-fab-test.js
@@ -5,6 +5,7 @@ const data = await fetch('./../../icons.json').then(res => res.json());
 
 data.icons.forEach(icon => {
   const fab = document.createElement('audiosync-fab');
+  fab.id = icon.name;
   svgIcon(icon.name).then(svg => fab.appendChild(svg));
   fab.setAttribute('color', generateRandomHexCode());
   fab.onClick(_ => {
@@ -17,16 +18,34 @@ data.icons.forEach(icon => {
   qs('body').appendChild(fab);
   
   setInterval(_ => {
+    if (document.hidden) return;
     fab.toggleAttribute('noshadow');
   },5000);
 
   setInterval(_ => {
+    if (document.hidden) return;
     fab.setAttribute('color', generateRandomHexCode());
   }, 65000);
 });
 
+// testing disabled attribute
+function randomLowerThan(maxValue) {
+  return Math.floor(Math.random() * maxValue);
+}
 
+let last;
+setInterval(_ =>  {
+  if (document.hidden) return;
+  const fabs = qsa('audiosync-fab');
+  const ndx = randomLowerThan(data.icons.length);
+  if (fabs[last]) {
+    fabs[last].removeAttribute('disabled');
+  }
+  fabs[ndx].toggleAttribute('disabled');
+  last = ndx;
+}, 5000);
 
+// cycle through showing / hiding each fab
 function iterateAndReset(maxValue) {
   let number = 0;
   let increment = 1;
@@ -35,6 +54,7 @@ function iterateAndReset(maxValue) {
   fabs[0].onScreen();
 
   async function iterate() {
+    if (document.hidden) return;
 
     await fabs[number].offScreen();
 
@@ -44,6 +64,10 @@ function iterateAndReset(maxValue) {
     }
 
     await fabs[number].onScreen();
+
+    if (fabs[number].hasAttribute('disabled') && fabs[number].hasAttribute('onScreen')) {
+      new Toast(`${fabs[number].id} fab is diabled`,4);
+    }
   }
   setInterval(iterate, 5000); 
 }
