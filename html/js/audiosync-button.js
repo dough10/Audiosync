96fb6cd77a7437999a13d41375363c923939d85d
dough10 <doughten.jimi@gmail.com>
2024-05-14 05:30:52 -0500

    unnessacery body css

--- html/js/audiosync-button.js
+++ html/js/audiosync-button.js
@@ -19,25 +19,6 @@ class AudioSyncButton extends HTMLElement {
     
     const sheet = document.createElement('style');
     sheet.textContent = `
-    body {
-      position: absolute;
-      top:0;
-      bottom:0;
-      left:0;
-      right:0;
-      background: var(--background-color);
-      font-family: var(--font-family);
-      font-size: 13px;
-      -webkit-tap-highlight-color: rgba(0,0,0,0);
-      user-select: none;
-      overflow-x: hidden;
-      overflow-y: hidden;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      padding:0;
-      margin:0;
-    }
     .button {
       display: inline-flex;
       min-width: 5.14em;
