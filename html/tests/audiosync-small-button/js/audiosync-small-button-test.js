faaa49cfc75db3f2b2ac93ce531ff10e03458bed
dough10 <doughten.jimi@gmail.com>
2024-05-17 18:36:52 -0500

    upodate tests

--- html/tests/audiosync-small-button/js/audiosync-small-button-test.js
+++ html/tests/audiosync-small-button/js/audiosync-small-button-test.js
@@ -1,19 +1,22 @@
-import {qs, svgIcon, generateRandomHexCode, Toast} from '../../../js/helpers.js';
+import {qs, qsa, svgIcon, generateRandomHexCode, Toast} from '../../../js/helpers.js';
 
 const data = await fetch('./../../icons.json').then(res => res.json());
 
 data.icons.forEach(icon => {
   const button = document.createElement('audiosync-small-button');
+  button.id = icon.name;
   svgIcon(icon.name).then(svg => button.appendChild(svg));
   button.color(generateRandomHexCode());
   qs('body').appendChild(button);
   button.onClick(_ => new Toast(`${icon.name} small button clicked`,1));
-
-  setInterval(_ => {
-    button.color(generateRandomHexCode());
-  }, 5000);
-
-  setInterval(_ => {
-    button.toggleAttribute('disabled');
-  }, 20000);
 });
+
+let last;
+setInterval(_ => {
+  const buttons = qsa('audiosync-small-button');
+  const ndx = Math.floor(Math.random() * data.icons.length);
+  buttons[ndx].toggleAttribute('disabled');
+  new Toast(`${buttons[ndx].id} button is disabled`)
+  if (buttons[last]) buttons[last].removeAttribute('disabled');
+  last = ndx;
+}, 15000);
--- html/tests/svgIcon/js/svgIcon-test.js
+++ html/tests/svgIcon/js/svgIcon-test.js
@@ -1,29 +1,29 @@
-import {qs, generateRandomHexCode, fillButton} from '../../../js/helpers.js';
+import {qs, generateRandomHexCode, fillButton, parseCSS, objectToCSS, generateRandomString} from '../../../js/helpers.js';
 
 const data = await fetch('./../../icons.json').then(res => res.json());
 
 
-data.icons.forEach(async icon => {
+data.icons.forEach(icon => {
+  const className = generateRandomString()
+
   const color = generateRandomHexCode()
-  const css = `
-  .${icon.name} {
-    color: ${color};
-    display: flex;
-    flex-direction: row;
-    margin-right:16px;
-  }
-  .${icon.name} > :first-child {
-    margin-right: 16px;
-  }
-  .${icon.name} > :nth-child(2) {
-    display: flex;
-    align-items: center;
-    margin-right:8px;
-  }
-  `;
-  const style = qs('style').textContent;
-  qs('style').textContent = style + css;
-  const svg = fillButton(icon.name, icon.name);
-  svg.classList.add(icon.name);
-  qs('body').appendChild(svg);
+  const style = parseCSS(qs('style').textContent);
+  style[`.${className}`] = {
+    'color': color,
+    'display': 'flex',
+    'flex-direction': 'row',
+    'margin-right':'16px'
+  };
+  style[`.${className} > :first-child`] = {
+    'margin-right': '16px'
+  };
+  style[`.${className} > :nth-child(2)`] = {
+    'display': 'flex',
+    'align-items': 'center',
+    'margin-right':'8px'
+  };
+  qs('style').textContent = objectToCSS(style);
+  const el = fillButton(icon.name, icon.name);
+  el.classList.add(className);
+  qs('body').appendChild(el);
 });
