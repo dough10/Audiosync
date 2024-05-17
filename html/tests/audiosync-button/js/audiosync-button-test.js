eb7add6467c9098c3db6c5baa47741ef62120681
dough10 <doughten.jimi@gmail.com>
2024-05-17 14:12:03 -0500

    test updates

--- html/tests/audiosync-button/js/audiosync-button-test.js
+++ html/tests/audiosync-button/js/audiosync-button-test.js
@@ -1,4 +1,4 @@
-import {fillButton, generateRandomHexCode, Toast} from '../../../js/helpers.js';
+import {qs, qsa, fillButton, generateRandomHexCode, Toast} from '../../../js/helpers.js';
 
 const data = await fetch('./../../icons.json').then(res => res.json());
 
@@ -6,17 +6,22 @@ data.icons.forEach(icon => {
   const button = document.createElement('audiosync-button');
   button.appendChild(fillButton(icon.name, icon.name));
   button.setAttribute('color', generateRandomHexCode());
-  button.onClick(_ => new Toast(`${icon.name} button clicked`,1));
+  button.onClick(_ => {
+    new Toast(`${icon.name} button clicked`,1);
+    const svg = qs('svg', button);
+    svg.classList.add('spinning');
+    setTimeout(_ => svg.classList.remove('spinning'), 1500);
+  });
   document.querySelector('body').appendChild(button);
-
-  // color & shadow
-  setInterval(_ => {
-    button.setAttribute('color', generateRandomHexCode());
-    button.toggleAttribute('noshadow');
-  }, 5000);
-
-  // disabled
-  setInterval(_ => {
-    button.toggleAttribute('disabled');
-  }, 20000);
 });
+
+let last;
+setInterval(_ => {
+  const buttons = qsa('audiosync-button');
+  const ndx = Math.floor(Math.random() * data.icons.length);
+  buttons[ndx].toggleAttribute('disabled');
+  if (buttons[last]) {
+    buttons[last].removeAttribute('disabled');
+  }
+  last = ndx;
+}, 5000);
