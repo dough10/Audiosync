58e0aec737b4100429ed5a64dae6788e7166f191
dough10 <doughten.jimi@gmail.com>
2024-05-18 19:30:46 -0500

    more numbers

--- html/js/audiosync-button.js
+++ html/js/audiosync-button.js
@@ -53,8 +53,8 @@ class AudioSyncButton extends HTMLElement {
         "box-shadow": "0 8px 10px 1px rgba(0,0,0,.14), 0 3px 14px 2px rgba(0,0,0,.12), 0 5px 5px -3px rgba(0,0,0,.4)",
         "content": "' '",
         "position": "absolute",
-        "top": "0",
-        "left": "0"
+        "top": 0,
+        "left": 0
       },
       ".button:hover:after": {
         "opacity": 1
