f6b368376c2439f62812e86be4bef3a632733524
dough10 <doughten.jimi@gmail.com>
2024-05-21 22:55:24 -0500

    looks for audioplayer

--- html/js/audiosync-dialog.js
+++ html/js/audiosync-dialog.js
@@ -1,4 +1,4 @@
-import {fadeIn, fadeOut, animateElement, objectToCSS} from './helpers.js';
+import {qs, fadeIn, fadeOut, animateElement, objectToCSS, parseCSS} from './helpers.js';
 
 /**
  * dialog box 
@@ -11,40 +11,40 @@ class AudioSyncDialog extends HTMLElement {
     super();
     const cssObj = {
       "#click-blocker": {
-        "position": "absolute",
-        "top": 0,
-        "bottom": 0,
-        "left": 0,
-        "right": 0,
+        position: "absolute",
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
         "background-color": "rgba(0, 0, 0, 0.4)",
         "pointer-events": "all",
         "z-index": 3
       },
       ".allow-clicks": {
         "pointer-events": "none",
-        "display": "none"
+        display: "none"
       },
       ".dialog": {
-        "position": "absolute",
-        "top": "150px",
-        "bottom": "150px",
-        "left": "25px",
-        "right": "25px",
-        "color": "#333333",
-        "padding": "24px",
-        "background": "#ffffff",
+        position: "absolute",
+        top: "150px",
+        bottom: "150px",
+        left: "25px",
+        right: "25px",
+        color: "#333333",
+        padding: "24px",
+        background: "#ffffff",
         "border-radius": "3px",
         "box-shadow": "0 2px 2px 0 rgba(0,0,0,0.14),0 1px 5px 0 rgba(0,0,0,0.12),0 3px 1px -2px rgba(0,0,0,0.2)",
         "text-align": "center",
         "z-index": 4,
-        "transform": "scale3d(0, 0, 0)",
+        transform: "scale3d(0, 0, 0)",
         "max-width": "650px"
       },
       ".small": {
-        "top": "300px",
-        "bottom": "300px",
-        "left": "100px",
-        "right": "100px"
+        top: "300px",
+        bottom: "300px",
+        left: "100px",
+        right: "100px"
       }
     };
 
@@ -59,12 +59,14 @@ class AudioSyncDialog extends HTMLElement {
     this.dialog.classList.add('dialog');
     this.dialog.appendChild(document.createElement('slot'));
     
-    const shadow = this.attachShadow({mode: "open"});
+    this.attachShadow({mode: "open"});
     [
       sheet,
       this.blocker,
       this.dialog
-    ].forEach(el => shadow.appendChild(el));
+    ].forEach(el => this.shadowRoot.appendChild(el));
+
+    setInterval(this._playerDisplayToggle.bind(this), 50);
   }
 
   /**
@@ -73,6 +75,23 @@ class AudioSyncDialog extends HTMLElement {
   connectedCallback() {
     this.hasAttribute('small') ? this.dialog.classList.add('small') : this.dialog.classList.remove('small');
     this.setAttribute('opened', 0);
+  }
+
+  /**
+   * toggle height of click blocked blocking audio mini player
+   * 
+   * @returns {void}
+   */
+  _playerDisplayToggle() {
+    if (!qs('audiosync-player')) return;
+    const styles = parseCSS(qs('style', this.shadowRoot).textContent);
+    if (qs('audiosync-player').hasAttribute('playing')) {
+      styles['#click-blocker'].bottom = '72px';
+    } else {
+      styles['#click-blocker'].bottom = 0;
+
+    }
+    qs('style', this.shadowRoot).textContent = objectToCSS(styles);
   }
 
   /**
