d626973acdc992ea01ad2767c21d02cc8bddcd6a
dough10 <doughten.jimi@gmail.com>
2024-05-25 20:07:52 -0500

    same thing less code

--- html/js/audiosync-dialog.js
+++ html/js/audiosync-dialog.js
@@ -1,12 +1,9 @@
-import {qs, ce, fadeIn, fadeOut, animateElement, objectToCSS, parseCSS} from './helpers.js';
+import {qs, ce, objectToCSS, parseCSS} from './helpers.js';
 
 /**
  * dialog box 
  */
 class AudioSyncDialog extends HTMLElement {
-  static get observedAttributes() {
-    return ['small'];
-  }
   constructor() {
     super();
     const cssObj = {
@@ -18,18 +15,16 @@ class AudioSyncDialog extends HTMLElement {
         right: 0,
         "background-color": "rgba(0, 0, 0, 0.4)",
         "pointer-events": "all",
-        "z-index": 3
+        "z-index": 3,
+        'will-change': 'opacity',
+        'transition': 'opacity 300ms cubic-bezier(.33,.17,.85,1.1)'
       },
       ".allow-clicks": {
         "pointer-events": "none",
-        display: "none"
+        opacity:0
       },
       ".dialog": {
-        position: "absolute",
-        top: "150px",
-        bottom: "150px",
-        left: "25px",
-        right: "25px",
+        position: "fixed",
         color: "#333333",
         padding: "24px",
         background: "#ffffff",
@@ -37,14 +32,14 @@ class AudioSyncDialog extends HTMLElement {
         "box-shadow": "0 2px 2px 0 rgba(0,0,0,0.14),0 1px 5px 0 rgba(0,0,0,0.12),0 3px 1px -2px rgba(0,0,0,0.2)",
         "text-align": "center",
         "z-index": 4,
-        transform: "scale3d(0, 0, 0)",
-        "max-width": "650px"
+        'will-change': 'transform',
+        transition: 'transform 300ms cubic-bezier(.33,.17,.85,1.1)',
+        transform: "translate(-50%, -50%) scale3d(0,0,0)",
+        "max-width": "650px",
+        'min-width': '310px'
       },
-      ".small": {
-        top: "300px",
-        bottom: "290px",
-        left: "100px",
-        right: "100px"
+      '.open': {
+        transform: 'translate(-50%, -50%) scale3d(1,1,1)'
       }
     };
 
@@ -54,10 +49,12 @@ class AudioSyncDialog extends HTMLElement {
     this.blocker = ce('div');
     this.blocker.id = 'click-blocker';
     this.blocker.classList.add('allow-clicks');
+    this.blocker.style.display = 'none';
 
     this.dialog = ce('div');
     this.dialog.classList.add('dialog');
     this.dialog.appendChild(ce('slot'));
+    this.dialog.addEventListener('transitionend', _ => this.toggleAttribute('open'))
     
     this.attachShadow({mode: "open"});
     [
@@ -67,14 +64,6 @@ class AudioSyncDialog extends HTMLElement {
     ].forEach(el => this.shadowRoot.appendChild(el));
 
     setInterval(this._playerDisplayToggle.bind(this), 50);
-  }
-
-  /**
-   * element connected to DOM
-   */
-  connectedCallback() {
-    this.hasAttribute('small') ? this.dialog.classList.add('small') : this.dialog.classList.remove('small');
-    this.setAttribute('opened', 0);
   }
 
   /**
@@ -98,31 +87,30 @@ class AudioSyncDialog extends HTMLElement {
    * open the dialog
    */
   async open() {
-    fadeIn(this.blocker);
-    this.blocker.classList.remove('allow-clicks');
-    await animateElement(this.dialog, 'scale3d(1, 1, 1)', 250);
-    this.setAttribute('opened', 1);
+    const tend = _ => {
+      this.blocker.removeEventListener('transitionend', tend);
+    };
+    this.dialog.addEventListener('transitionend', tend);
+    requestAnimationFrame(_ => {
+      this.dialog.classList.add('open');
+      this.blocker.classList.remove('allow-clicks');
+      this.blocker.style.removeProperty('display');
+    });
   }
 
   /**
    * close the dialog
    */
   async close() {
-    fadeOut(this.blocker);
-    await animateElement(this.dialog, 'scale3d(0, 0, 0)', 250);
-    this.blocker.classList.add('allow-clicks');
-    this.setAttribute('opened', 0);
-  }
-
-  /**
-   * attribute has changed 
-   * 
-   * @param {String} name
-   * @param {Number} oldVal
-   * @param {Number} newVal
-   */
-  attributeChangedCallback(name, oldVal, newVal) {
-    this.hasAttribute('small') ? this.dialog.classList.add('small') : this.dialog.classList.remove('small');
+    const tend = _ => {
+      this.blocker.removeEventListener('transitionend', tend);
+      this.blocker.style.display = 'none';
+    };
+    this.dialog.addEventListener('transitionend', tend);
+    requestAnimationFrame(_ => {
+      this.dialog.classList.remove('open');
+      this.blocker.classList.add('allow-clicks');
+    });
   }
 }
 customElements.define('audiosync-dialog', AudioSyncDialog);
