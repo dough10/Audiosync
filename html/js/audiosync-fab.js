c6d4c3b0b8a8f170304c1f3b484549ceed6e94e8
dough10 <doughten.jimi@gmail.com>
2024-05-29 13:07:35 -0500

    css

--- html/js/audiosync-fab.js
+++ html/js/audiosync-fab.js
@@ -57,7 +57,7 @@ class FloatingActionButton extends HTMLElement {
         "border-radius": "50%",
         transition: "opacity 150ms cubic-bezier(.33,.17,.85,1.1)",
         "box-shadow": "0 8px 10px 1px rgba(0,0,0,.14), 0 3px 14px 2px rgba(0,0,0,.12), 0 5px 5px -3px rgba(0,0,0,.4)",
-        content: " ",
+        content: "' '",
         position: "absolute",
         top: 0,
         left: 0
