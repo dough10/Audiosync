e1d006a7c878404779f40c0f8b528b08b0a5fad5
dough10 <doughten.jimi@gmail.com>
2024-05-31 22:49:02 -0500

    more radius / nopad attribute

--- html/js/audiosync-dialog.js
+++ html/js/audiosync-dialog.js
@@ -4,8 +4,14 @@ import {qs, ce, objectToCSS, parseCSS} from './helpers.js';
  * dialog box 
  */
 class AudioSyncDialog extends HTMLElement {
+  static get observedAttributes() {
+    return ['nopad'];
+  }
   constructor() {
     super();
+
+    this.close = this.close.bind(this);
+
     const cssObj = {
       "#click-blocker": {
         position: "absolute",
@@ -28,7 +34,7 @@ class AudioSyncDialog extends HTMLElement {
         color: "#333333",
         padding: "24px",
         background: "#ffffff",
-        "border-radius": "3px",
+        "border-radius": "10px",
         "box-shadow": "0 2px 2px 0 rgba(0,0,0,0.14),0 1px 5px 0 rgba(0,0,0,0.12),0 3px 1px -2px rgba(0,0,0,0.2)",
         "text-align": "center",
         "z-index": 4,
@@ -36,7 +42,8 @@ class AudioSyncDialog extends HTMLElement {
         transition: 'transform 300ms cubic-bezier(.33,.17,.85,1.1)',
         transform: "translate(-50%, -50%) scale3d(0,0,0)",
         "max-width": "650px",
-        'min-width': '310px'
+        'min-width': '310px',
+        overflow: 'hidden'
       },
       '.open': {
         transform: 'translate(-50%, -50%) scale3d(1,1,1)'
@@ -62,6 +69,10 @@ class AudioSyncDialog extends HTMLElement {
       this.blocker,
       this.dialog
     ].forEach(el => this.shadowRoot.appendChild(el));
+  }
+
+  setOrigin(x,y) {
+    this.dialog.style.transformOrigin = `${x}px ${y}px`;
   }
 
   /**
@@ -92,6 +103,25 @@ class AudioSyncDialog extends HTMLElement {
       this.dialog.classList.remove('open');
       this.blocker.classList.add('allow-clicks');
     });
+  }
+
+  /**
+   * attribute has changed 
+   * 
+   * @param {String} name
+   * @param {Number} oldVal
+   * @param {Number} newVal
+   */
+  attributeChangedCallback(name, oldVal, newVal) {
+    if (this.hasAttribute('nopad')) {
+      const styles = parseCSS(qs('style', this.shadowRoot).textContent);
+      styles['.dialog'].padding = 0;
+      qs('style', this.shadowRoot).textContent = objectToCSS(styles); 
+    } else {
+      const styles = parseCSS(qs('style', this.shadowRoot).textContent);
+      styles['.dialog'].padding = '24px';
+      qs('style', this.shadowRoot).textContent = objectToCSS(styles); 
+    }
   }
 }
 customElements.define('audiosync-dialog', AudioSyncDialog);
