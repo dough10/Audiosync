b1c6b4145b4e171e56e6bf004803b239200c01ac
dough10 <doughten.jimi@gmail.com>
2024-06-12 13:51:50 -0500

    update tests

--- html/tests/audiosync-button/js/audiosync-button-test.js
+++ html/tests/audiosync-button/js/audiosync-button-test.js
@@ -1,18 +1,15 @@
-import {qs, qsa, fillButton, generateRandomHexCode, Toast} from '../../../js/helpers.js';
+import {qs, qsa, ce, fillButton, generateRandomHexCode, Toast} from '../../../js/helpers.js';
 
 const data = await fetch('./../../icons.json').then(res => res.json());
 
 data.icons.forEach(icon => {
-  const button = document.createElement('audiosync-button');
+  const button = ce('audiosync-button');
   button.appendChild(fillButton(icon.name, icon.name));
   button.setAttribute('color', generateRandomHexCode());
   button.onClick(_ => {
     new Toast(`${icon.name} button clicked`,1);
-    const svg = qs('svg', button);
-    svg.classList.add('spinning');
-    setTimeout(_ => svg.classList.remove('spinning'), 1500);
   });
-  document.querySelector('body').appendChild(button);
+  qs('body').appendChild(button);
 });
 
 let last;
