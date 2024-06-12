51e6bc0514cfe2ca50de9bf6dd69a937c08ed2ed
dough10 <doughten.jimi@gmail.com>
2024-06-12 02:28:03 -0500

    added an input in the dialog

--- html/tests/audiosync-dialog/js/audiosync-dialog-test.js
+++ html/tests/audiosync-dialog/js/audiosync-dialog-test.js
@@ -1,7 +1,11 @@
 import {qs, ce, sleep} from '../../../js/helpers.js';
 
+const INPUT = ce('input');
+INPUT.type = 'text';
+INPUT.toggleAttribute('autoFocus');
 
 const DIALOG = ce('audiosync-dialog');
 qs('body').appendChild(DIALOG);
-await sleep(5000);
+DIALOG.appendChild(INPUT);
+await sleep(500);
 DIALOG.open();
