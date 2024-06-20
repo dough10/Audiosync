53ddf3e49b0d807dfbfbc1f39b2cef0c47de186d
dough10 <doughten.jimi@gmail.com>
2024-06-19 21:33:11 -0500

    renamed for better readability

--- lib/cue_from_discogs.py
+++ lib/cue_from_discogs.py
@@ -5,6 +5,8 @@ import discogs_client
 
 file_path = os.path.abspath(__file__)
 script_folder = os.path.dirname(file_path)
+
+
 config_path = os.path.join(script_folder, '..', 'config.json')
 with open(config_path, 'r') as j:
   config = json.load(j)
@@ -24,17 +26,26 @@ def string_to_boolean(s):
     raise ValueError("Invalid boolean string")
 
 def save_cue(release, mp3_path, gapless):
+
   basename = os.path.basename(mp3_path)
+
   name = os.path.splitext(mp3_path)[0]
+
   cue_file_path = os.path.join(os.path.dirname(mp3_path), f'{name}.cue')
+
   file_extension = os.path.splitext(mp3_path)[1].replace('.', '')
+
   artist_names = [artist.name for artist in release.artists]
+
   releaseID = os.path.join(os.path.dirname(mp3_path), 'releaseid.txt')
+
   if not os.path.exists(releaseID):
     with open(releaseID, 'w') as txt:
       txt.write(f'{release.id}\n{gapless}')
+
   if os.path.exists(cue_file_path):
     return
+  
   with open(cue_file_path, 'w', encoding='utf-8') as cue_file:
     cue_file.write(f'CATALOG "{release.data.get('barcode')}"\n')
     cue_file.write(f'PERFORMER "{artist_names[0]}"\n')
@@ -75,7 +86,7 @@ def add_durations(durations):
   total_time = f"{total_minutes:02d}:{total_seconds:02d}"
   return total_time
 
-def create_cue(release_id, mp3_path, gapless):
+def get_discogs_data(release_id, mp3_path, gapless):
   d = discogs_client.Client('ExampleApplication/0.1', user_token=config['discogs_token'])
   release = d.release(release_id)
   save_cue(release, mp3_path, gapless)
@@ -83,13 +94,13 @@ def create_cue(release_id, mp3_path, gapless):
 def cue_from_releaseid(releaseID, file_path, changes):
   try:
     rid = open(releaseID, 'r').read().split('\n')
-    create_cue(rid[0], file_path, rid[1])
+    get_discogs_data(rid[0], file_path, rid[1])
     changes['playlist_created'] += 1
   except:
     log(f'Error reading releaseid {releaseID}')
 
 if __name__ == "__main__":
-  create_cue(
+  get_discogs_data(
     '585587',
     'z:\\Music\\Unsorted\\Other\\Black Uhuru\\Guess who\'s comming for dinner\\Black Uhuru - Guess Whos Coming To.mp3', 
     'false'
