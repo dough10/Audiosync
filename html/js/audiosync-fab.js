2f0f565997b5ff9517ac6ecec8b3e80e459a121e
dough10 <doughten.jimi@gmail.com>
2024-06-28 18:47:01 -0500

    remove double quotes

--- html/js/audiosync-fab.js
+++ html/js/audiosync-fab.js
@@ -1,4 +1,4 @@
-import {qs, ce, createRipple, hexToRgba, getCSSVariableValue, convertToHex, getContrastColor, parseCSS, objectToCSS} from './helpers.js';
+import {qs, ce, createRipple, convertToHex, getContrastColor, objectToCSS} from './helpers.js';
 
 /**
  * pages
@@ -9,110 +9,104 @@ class FloatingActionButton extends HTMLElement {
   }
   constructor() {
     super();
-    this.attachShadow({mode: "open"});
-    // color higherarchy 
-    // color attribute > css '--main-color' variable > white
-    const COLOR = convertToHex(getCSSVariableValue('--pop-color'));
-    
-    // contrasting text color 
-    const CONTRAST_COLOR = getContrastColor(COLOR);
+    this.attachShadow({mode: 'open'});
 
     const CSS_OBJECT = {
-      ".fab": {
-        overflow: "hidden",
-        color: CONTRAST_COLOR,
+      '.fab': {
+        overflow: 'hidden',
+        color: 'var(--contrast-color)',
         background: 'var(--pop-color)',
-        display: "flex",
-        "flex-direction": "column",
-        "align-items": "center",
-        "justify-content": "center",
-        "border-radius": "50%",
-        "box-sizing": "border-box",
-        cursor: "pointer",
-        height: "56px",
-        "min-width": 0,
-        outline: "none",
-        padding: "16px",
-        width: "56px",
-        "-webkit-tap-highlight-color": "transparent",
-        "box-shadow": "0 4px 5px 0 rgba(0,0,0,0.14),0 1px 10px 0 rgba(0,0,0,0.12),0 2px 4px -1px rgba(0,0,0,0.4)",
+        display: 'flex',
+        'flex-direction': 'column',
+        'align-items': 'center',
+        'justify-content': 'center',
+        'border-radius': '50%',
+        'box-sizing': 'border-box',
+        cursor: 'pointer',
+        height: '56px',
+        'min-width': 0,
+        outline: 'none',
+        padding: '16px',
+        width: '56px',
+        '-webkit-tap-highlight-color': 'transparent',
+        'box-shadow': '0 4px 5px 0 rgba(0,0,0,0.14),0 1px 10px 0 rgba(0,0,0,0.12),0 2px 4px -1px rgba(0,0,0,0.4)',
         transition: 'var(--button-bg-animation)'
       },
-      ".fab > *": {
-        "pointer-events": "none"
+      '.fab > *': {
+        'pointer-events': 'none'
       },
-      ".fab:after": {
-        display: "inline-block",
-        "z-index": -1,
-        width: "100%",
-        height: "100%",
+      '.fab:after': {
+        display: 'inline-block',
+        'z-index': -1,
+        width: '100%',
+        height: '100%',
         opacity: 0,
-        "border-radius": "50%",
-        transition: "opacity 150ms cubic-bezier(.33,.17,.85,1.1)",
-        "box-shadow": "0 8px 10px 1px rgba(0,0,0,.14), 0 3px 14px 2px rgba(0,0,0,.12), 0 5px 5px -3px rgba(0,0,0,.4)",
-        content: "' '",
-        position: "absolute",
+        'border-radius': '50%',
+        transition: 'opacity 150ms cubic-bezier(.33,.17,.85,1.1)',
+        'box-shadow': '0 8px 10px 1px rgba(0,0,0,.14), 0 3px 14px 2px rgba(0,0,0,.12), 0 5px 5px -3px rgba(0,0,0,.4)',
+        content: '" "',
+        position: 'absolute',
         top: 0,
         left: 0
       },
-      ".fab:hover:after": {
+      '.fab:hover:after': {
         opacity: 1
       },
-      ".fab:hover:active:after": {
+      '.fab:hover:active:after': {
         opacity: 0
       },
-      ".fab[disabled]": {
-        background: "rgba(84, 84, 84, 0.4)",
-        color: "#ffffff",
-        'box-shadow': "none",
-        cursor: "none",
-        "pointer-events": "none"
+      '.fab[disabled]': {
+        background: 'rgba(84, 84, 84, 0.4)',
+        color: '#ffffff',
+        'box-shadow': 'none',
+        cursor: 'none',
+        'pointer-events': 'none'
       },
-      ".fab[disabled]:active, .fab[disabled]:hover, .fab[disabled]:active:hover": {
-        "box-shadow": "none",
-        "background-color": "rgba(0, 0, 0, 0.178)"
+      '.fab[disabled]:active, .fab[disabled]:hover, .fab[disabled]:active:hover': {
+        'box-shadow': 'none',
+        'background-color': 'rgba(0, 0, 0, 0.178)'
       },
-      ".fab[noshadow], .fab[noshadow]:hover, .fab[noshadow]:hover:after, .fab[noshadow]:after": {
-        "box-shadow": "none"
+      '.fab[noshadow], .fab[noshadow]:hover, .fab[noshadow]:hover:after, .fab[noshadow]:after': {
+        'box-shadow': 'none'
       },
-      ".fab[noshadow]:active": {
-        "box-shadow": "0 2px 2px 0 rgba(0,0,0,0.14),0 1px 5px 0 rgba(0,0,0,0.12),0 3px 1px -2px rgba(0,0,0,0.2)"
+      '.fab[noshadow]:active': {
+        'box-shadow': '0 2px 2px 0 rgba(0,0,0,0.14),0 1px 5px 0 rgba(0,0,0,0.12),0 3px 1px -2px rgba(0,0,0,0.2)'
       },
-      "@keyframes ripple-animation": {
+      '@keyframes ripple-animation': {
         to: {
-          transform: "scale(4)",
+          transform: 'scale(4)',
           opacity: 0
         }
       },
-      ".ripple-effect": {
-        position: "absolute",
-        "border-radius": "50%",
-        background: hexToRgba(CONTRAST_COLOR),
-        animation: "ripple-animation 0.7s linear"
+      '.ripple-effect': {
+        position: 'absolute',
+        'border-radius': '50%',
+        background: 'var(--contrast-color)',
+        animation: 'ripple-animation 0.7s linear'
       }
     };
 
     const ELEMENT_STYLES = ce('style');
     ELEMENT_STYLES.textContent = objectToCSS(CSS_OBJECT);
     
-    this.fab = ce('div');
-    this.fab.classList.add('fab');
-    this.fab.appendChild(ce('slot'));
-    this.fab.addEventListener('transitionend', _ => {
-      if (this.fab.classList.contains('onscreen')) {
+    this._fab = ce('div');
+    this._fab.classList.add('fab');
+    this._fab.appendChild(ce('slot'));
+    this._fab.addEventListener('transitionend', _ => {
+      if (this._fab.classList.contains('onscreen')) {
         this.toggleAttribute('onscreen');
         return;
       }
       this.removeAttribute('onscreen');
     })
-    this.fab.addEventListener('click', e => {
+    this._fab.addEventListener('click', e => {
       if (this.hasAttribute('disabled')) return;
       createRipple(e);
     });
     
     [
       ELEMENT_STYLES,
-      this.fab
+      this._fab
     ].forEach(el => this.shadowRoot.appendChild(el));
   }
 
@@ -127,39 +121,22 @@ class FloatingActionButton extends HTMLElement {
     if (['disabled','noshadow'].includes(name)) {
       // reflect attribute changes to the button element
       if (this.hasAttribute(name)) {
-        this.fab.setAttribute(name, '');
+        this._fab.setAttribute(name, '');
       } else {
-        this.fab.removeAttribute(name);
+        this._fab.removeAttribute(name);
       }
     } else if (name === 'color') {
-      if (newVal === null) return;
-      
-      // capture current styles and remove .new-color and .ripple-effect classes
-      const ELEMENT_STYLES = parseCSS(qs('style', this.shadowRoot).textContent);
-      
-      // background-color in hex format
-      const HEX_COLOR = convertToHex(newVal);
-      
-      // text / ripple color
-      const CONTRAST_COLOR = getContrastColor(HEX_COLOR);
-      
-      // create the new style 
-      ELEMENT_STYLES['.new-color'] = {
-        'background-color': HEX_COLOR, 
-        'color': CONTRAST_COLOR
-      };
-      ELEMENT_STYLES['.ripple-effect'].background = hexToRgba(CONTRAST_COLOR);
-      
-      // update styles
-      qs('style', this.shadowRoot).textContent = objectToCSS(ELEMENT_STYLES);         
-      
-      // set the new class
-      this.fab.classList.add('new-color');
+      const color = convertToHex(newVal);
+      const contrast = getContrastColor(color);
+      this.style.setProperty('--pop-color', color);
+      this.style.setProperty('--contrast-color', contrast);
     }
   }
 
   /**
    * add's an click event listener
+   * @function
+   * @public
    * 
    * @param {Function} cb callback function
    */
@@ -168,7 +145,7 @@ class FloatingActionButton extends HTMLElement {
       if (this.hasAttribute('disabled')) return;
       cb(e);
     }
-    this.fab.addEventListener('click', CALLBACK);
+    this._fab.addEventListener('click', CALLBACK);
   }
 
 }
