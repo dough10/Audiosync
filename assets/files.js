1eaa2a4b9a73f2f36ef40c58bfa8622fddfcdd6d
dough10 <doughten.jimi@gmail.com>
2024-06-28 18:47:18 -0500

    added audio player

--- assets/files.js
+++ assets/files.js
@@ -1,5 +1,6 @@
 module.exports = [
   'make-docs',
+  'html/js/audiosync-player/audiosync-player',
   'html/js/audiosync-podcasts/audiosync-podcasts',
   'html/js/audiosync-dialog/audiosync-dialog',
   'html/js/helpers'
