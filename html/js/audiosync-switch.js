86bb9ccf1ac1dc65d7fa2b296c311d5b7388387a
dough10 <doughten.jimi@gmail.com>
2024-06-30 19:30:07 -0500

    ' > "

--- html/js/audiosync-switch.js
+++ html/js/audiosync-switch.js
@@ -12,111 +12,112 @@ class AudioSyncSwitch extends HTMLElement {
     super();
 
     const CSS_OBJECT = {
-      ".audiosync-switch": {
-        "z-index": 0,
-        "position": "relative",
-        "display": "inline-block",
-        "color": "var(--text-color)",
-        "font-family": "var(--font-family)",
-        "font-size": "16px",
-        "line-height": 1.5,
-        "width": "95%",
-        "margin": "8px"
+      '.audiosync-switch': {
+        'z-index': 0,
+        'position': 'relative',
+        'display': 'inline-block',
+        'color': 'var(--text-color)',
+        'font-family': 'var(--font-family)',
+        'font-size': '16px',
+        'line-height': 1.5,
+        'width': '450px',
+        'margin': '8px'
       },
-      ".audiosync-switch > input": {
-        "appearance": "none",
-        "-moz-appearance": "none",
-        "-webkit-appearance": "none",
-        "z-index": -1,
-        "position": "absolute",
-        "right": "6px",
-        "top": "-8px",
-        "display": "block",
-        "margin": 0,
-        "border-radius": "50%",
-        "width": "40px",
-        "height": "40px",
-        "background-color": "rgba(0, 0, 0, 0.38)",
-        "outline": "none",
-        "opacity": 0,
-        "transform": "scale(1)",
-        "pointer-events": "none",
-        "transition": "opacity 0.3s 0.1s, transform 0.2s 0.1s"
+      '.audiosync-switch > input': {
+        'appearance': 'none',
+        '-moz-appearance': 'none',
+        '-webkit-appearance': 'none',
+        'z-index': -1,
+        'position': 'absolute',
+        'right': '6px',
+        'top': '-8px',
+        'display': 'block',
+        'margin': 0,
+        'border-radius': '50%',
+        'width': '40px',
+        'height': '40px',
+        'background-color': 'rgba(0, 0, 0, 0.38)',
+        'outline': 'none',
+        'opacity': 0,
+        'transform': 'scale(1)',
+        'pointer-events': 'none',
+        'transition': 'opacity 0.3s 0.1s, transform 0.2s 0.1s'
       },
-      ".audiosync-switch > span": {
-        "display": "inline-block",
-        "width": "100%",
-        "cursor": "pointer",
-        "text-transform": "uppercase"
+      '.audiosync-switch > span': {
+        'display': 'inline-block',
+        'width': '100%',
+        'cursor': 'pointer',
+        'text-transform': 'uppercase',
+        'text-align': 'justify'
       },
-      ".audiosync-switch > span::before": {
-        "content": "\"\"",
-        "float": "right",
-        "display": "inline-block",
-        "margin": "5px 0 5px 10px",
-        "border-radius": "7px",
-        "width": "36px",
-        "height": "14px",
-        "background-color": "rgba(0, 0, 0, 0.38)",
-        "vertical-align": "top",
-        "transition": "background-color 0.2s, opacity 0.2s"
+      '.audiosync-switch > span::before': {
+        'content': '\'\'',
+        'float': 'right',
+        'display': 'inline-block',
+        'margin': '5px 0 5px 10px',
+        'border-radius': '7px',
+        'width': '36px',
+        'height': '14px',
+        'background-color': 'rgba(0, 0, 0, 0.38)',
+        'vertical-align': 'top',
+        'transition': 'background-color 0.2s, opacity 0.2s'
       },
-      ".audiosync-switch > span::after": {
-        "content": "\"\"",
-        "position": "absolute",
-        "top": "2px",
-        "right": "16px",
-        "border-radius": "50%",
-        "width": "20px",
-        "height": "20px",
-        "background-color": "rgb(var(--main-rgb))",
-        "box-shadow": "0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12)",
-        "transition": "background-color 0.2s, transform 0.2s"
+      '.audiosync-switch > span::after': {
+        'content': '\'\'',
+        'position': 'absolute',
+        'top': '2px',
+        'right': '16px',
+        'border-radius': '50%',
+        'width': '20px',
+        'height': '20px',
+        'background-color': 'rgb(var(--main-rgb))',
+        'box-shadow': '0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12)',
+        'transition': 'background-color 0.2s, transform 0.2s'
       },
-      ".audiosync-switch > input:checked": {
-        "right": "-10px",
-        "background-color": "rgb(var(--pop-rgb, 33, 150, 243))"
+      '.audiosync-switch > input:checked': {
+        'right': '-10px',
+        'background-color': 'rgb(var(--pop-rgb, 33, 150, 243))'
       },
-      ".audiosync-switch > input:checked + span::before": {
-        "background-color": "rgba(var(--pop-rgb), 0.6)"
+      '.audiosync-switch > input:checked + span::before': {
+        'background-color': 'rgba(var(--pop-rgb), 0.6)'
       },
-      ".audiosync-switch > input:checked + span::after": {
-        "background-color": "rgb(var(--pop-rgb, 33, 150, 243))",
-        "transform": "translateX(16px)"
+      '.audiosync-switch > input:checked + span::after': {
+        'background-color': 'rgb(var(--pop-rgb, 33, 150, 243))',
+        'transform': 'translateX(16px)'
       },
-      ".audiosync-switch:hover > input": {
-        "opacity": 0.04
+      '.audiosync-switch:hover > input': {
+        'opacity': 0.04
       },
-      ".audiosync-switch > input:focus": {
-        "opacity": 0.12
+      '.audiosync-switch > input:focus': {
+        'opacity': 0.12
       },
-      ".audiosync-switch:hover > input:focus": {
-        "opacity": 0.16
+      '.audiosync-switch:hover > input:focus': {
+        'opacity': 0.16
       },
-      ".audiosync-switch > input:active": {
-        "opacity": 1,
-        "transform": "scale(0)",
-        "transition": "transform 0s, opacity 0s"
+      '.audiosync-switch > input:active': {
+        'opacity': 1,
+        'transform': 'scale(0)',
+        'transition': 'transform 0s, opacity 0s'
       },
-      ".audiosync-switch > input:active + span::before": {
-        "background-color": "rgba(var(--pop-rgb), 0.6)"
+      '.audiosync-switch > input:active + span::before': {
+        'background-color': 'rgba(var(--pop-rgb), 0.6)'
       },
-      ".audiosync-switch > input:checked:active + span::before": {
-        "background-color": "rgba(0, 0, 0, 0.38)"
+      '.audiosync-switch > input:checked:active + span::before': {
+        'background-color': 'rgba(0, 0, 0, 0.38)'
       },
-      ".audiosync-switch > input:disabled": {
-        "opacity": 0
+      '.audiosync-switch > input:disabled': {
+        'opacity': 0
       },
-      ".audiosync-switch > input:disabled + span": {
-        "color": "rgb(var(--pure-material-onsurface-rgb, 0, 0, 0))",
-        "opacity": 0.38,
-        "cursor": "default"
+      '.audiosync-switch > input:disabled + span': {
+        'color': 'rgb(var(--pure-material-onsurface-rgb, 0, 0, 0))',
+        'opacity': 0.38,
+        'cursor': 'default'
       },
-      ".audiosync-switch > input:disabled + span::before": {
-        "background-color": "rgba(0, 0, 0, 0.38)"
+      '.audiosync-switch > input:disabled + span::before': {
+        'background-color': 'rgba(0, 0, 0, 0.38)'
       },
-      ".audiosync-switch > input:checked:disabled + span::before": {
-        "background-color": "rgba(var(--pop-rgb), 0.6)"
+      '.audiosync-switch > input:checked:disabled + span::before': {
+        'background-color': 'rgba(var(--pop-rgb), 0.6)'
       }
     };
 
@@ -136,7 +137,7 @@ class AudioSyncSwitch extends HTMLElement {
       SPAN_ELEMENT
     ].forEach(el => LABEL_TEXT.appendChild(el));
     
-    this.attachShadow({mode: "open"});
+    this.attachShadow({mode: 'open'});
     [
       ELEMENT_STYLES,
       LABEL_TEXT
