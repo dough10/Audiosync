6831eee564d25e61cbd107b7d7139da936409fc8
dough10 <doughten.jimi@gmail.com>
2024-07-09 08:04:02 -0500

    initial path

--- lib/select_folder.py
+++ lib/select_folder.py
@@ -6,11 +6,11 @@ import os
 def is_windows():
     return platform.system() == 'Windows'
 
-def select_folder():
+def select_folder(initial_dir=None):
   root = tk.Tk()
   root.withdraw()  # Hide the main window
 
-  folder_path = filedialog.askdirectory()
+  folder_path = filedialog.askdirectory(initialdir=initial_dir)
 
   root.destroy()
   if folder_path:
@@ -23,10 +23,11 @@ def select_folder():
         folder_path += "/"
       return folder_path
   else:
-    return False
+    return initial_dir
 
 if __name__ == "__main__":
-  folder_path = select_folder()
+  initial_dir = os.path.expanduser("~")
+  folder_path = select_folder(initial_dir)
   print(folder_path)
   if folder_path:
     print(os.path.join(folder_path, "Nine Inch Nails", "Quake", "Quake.m3u8"))
