33a54c1fa52c3351c7a0803a7b6e4c9c2d879644
dough10 <doughten.jimi@gmail.com>
2024-07-14 10:47:24 -0500

    check for cue before connecting to discogs

--- lib/cue_from_discogs.py
+++ lib/cue_from_discogs.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 import discogs_client
 
@@ -16,6 +17,10 @@ config_controler = Config()
 
 # do not change
 total_time = '00:00'
+
+def seconds_to_mmss(seconds):
+  minutes, seconds = divmod(seconds, 60)
+  return f"{int(minutes):02}:{int(seconds):02}"
 
 def string_to_boolean(s:str) -> bool:
   true_strings = ['true', 'yes', '1']
@@ -39,31 +44,37 @@ def save_cue(release:str, mp3_path:str, gapless:bool) -> None:
   file_extension = os.path.splitext(mp3_path)[1].replace('.', '')
 
   artist_names = [artist.name for artist in release.artists]
-
-  releaseID = os.path.join(os.path.dirname(mp3_path), 'releaseid.txt')
-
-  if not os.path.exists(releaseID):
-    with open(releaseID, 'w') as txt:
-      txt.write(f'{release.id}\n{gapless}')
-
-  if os.path.exists(cue_file_path):
-    return
+  
+  tracklist = [track for track in release.tracklist if track.duration]
   
   with open(cue_file_path, 'w', encoding='utf-8') as cue_file:
-    cue_file.write(f'CATALOG "{release.data.get('barcode')}"\n')
+    cue_file.write(f'REM COMMENT "created by Audiosync [{datetime.now().strftime("%Y%m%d")}]"\n')
+    cue_file.write(f'CATALOG "{release.data.get('barcode') or release.id}"\n')
     cue_file.write(f'PERFORMER "{artist_names[0]}"\n')
     cue_file.write(f'TITLE "{release.title}"\n')
     cue_file.write(f'FILE "{basename}" {file_extension.upper()}\n')
-    for ndx, track in enumerate(release.tracklist):
+    
+    if not len(tracklist):
+      cue_file.write('  TRACK 01 AUDIO\n')
+      cue_file.write(f'    TITLE "{release.title}"\n')
+      cue_file.write(f'    PERFORMER "{artist_names[0]}"\n')
+      cue_file.write('    INDEX 01 00:00:00\n')
+      
+    for track in tracklist:
+      
       performer_names = [artist.name for artist in track.artists]
       if len(performer_names) == 0:
         performer_names = [artist_names[0]]
-      if track.position and track.duration:
-        cue_file.write(f'  TRACK {str(ndx + 1).zfill(2)} AUDIO\n')
-        cue_file.write(f'    TITLE "{track.title}"\n')
-        cue_file.write(f'    PERFORMER "{performer_names[0]}"\n')
-        cue_file.write(f'    INDEX 01 {total_time}:00\n')
-        add_durations([total_time, f'0{track.duration}']) 
+
+      cue_file.write(f'  TRACK {str(tracklist.index(track) + 1).zfill(2)} AUDIO\n')
+      cue_file.write(f'    TITLE "{track.title}"\n')
+      cue_file.write(f'    PERFORMER "{performer_names[0]}"\n')
+      cue_file.write(f'    INDEX 01 {total_time}:00\n')
+      
+      
+      add_durations([total_time, f'0{track.duration}']) 
+
+
       if not string_to_boolean(gapless):
         add_durations([total_time, '00:02'])
 
@@ -97,15 +108,27 @@ def get_discogs_data(release_id:str, mp3_path:str, gapless:bool) -> None:
 def cue_from_releaseid(releaseID:str, file_path:str) -> None:
   """
   """
+  global total_time
+  
+  name = os.path.splitext(file_path)[0]
+  cue_file_path = os.path.join(os.path.dirname(file_path), f'{name}.cue')
+  if os.path.exists(cue_file_path):
+    return
+
   try:
     rid = open(releaseID, 'r').read().split('\n')
     get_discogs_data(rid[0], file_path, rid[1])
+    total_time = '00:00'
     change_log.playlist_created()
   except IndexError:
     log(f'Error reading releaseid {releaseID}')
 
 if __name__ == "__main__":
+  
+  path = 'Z:\\Music\\Unsorted\\House\\Bad Boy Bill\\Bangin\' the Box, Volume 1\\'
+  file = '01 Bangin the Box Volume 1'
+  
   cue_from_releaseid(
-    'z:\\Music\\Unsorted\\Other\\Black Uhuru\\Guess who\'s comming for dinner\\releaseid.txt', 
-    'z:\\Music\\Unsorted\\Other\\Black Uhuru\\Guess who\'s comming for dinner\\Black Uhuru - Guess Whos Coming To.mp3'
+    f'{path}releaseid.txt', 
+    f'{path}{file}.mp3'
   )
