0cb571669fea7634d5f1b3c94ed2e933d842261e
dough10 <doughten.jimi@gmail.com>
2024-07-14 10:43:47 -0500

    list comp

--- lib/new_files.py
+++ lib/new_files.py
@@ -1,15 +1,17 @@
 import os
 import datetime
-from lib.is_audio import is_audio_file
-from lib.old_date import old_date
+
+try:
+  from lib.is_audio import is_audio_file
+  from lib.old_date import old_date
+except ModuleNotFoundError:
+  from is_audio import is_audio_file
+  from old_date import old_date
 
 def list_of_new_files(path):
-  new_files = []
-
-  for root, dirs, files in os.walk(path):
-    for file in files:
-      file_path = os.path.join(root, file)
-      if is_audio_file(file_path) and old_date < datetime.datetime.fromtimestamp(os.path.getmtime(file_path)).date():
-        new_files.append(file_path)
-
+  new_files = [{'root': root, 'file': file, 'ext': os.path.splitext(file)[-1].lower()}
+               for root, _, files in os.walk(path)
+               for file in files
+               if is_audio_file(os.path.join(root, file)) and old_date < datetime.datetime.fromtimestamp(os.path.getmtime(os.path.join(root, file))).date()]
+  
   return new_files
