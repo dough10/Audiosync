0e397ef267d8683dbd65244e62aa601087910ebe
dough10 <doughten.jimi@gmail.com>
2024-09-10 19:23:42 -0500

    rm

--- assets/files.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-  'make-docs',
-  'html/js/audiosync-player/audiosync-player',
-  'html/js/audiosync-podcasts/audiosync-podcasts',
-  'html/js/audiosync-dialog/audiosync-dialog',
-  'html/js/helpers'
-]
